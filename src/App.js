515d500c441101a4a19501d46bcfe24cab833d60
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-16 21:54:50 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = 0.011
-const image = 'https://pbs.twimg.com/profile_images/1633927844400185344/2Qd7gsx1_400x400.jpg'
-const Title = 'Ape King'
-const supply = 772
+const ethAmount = 0
+const image = 'https://pbs.twimg.com/profile_images/1632803136011137025/zkuMrl4W_400x400.jpg'
+const Title = 'Earth.jpg'
+const supply = 111
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
