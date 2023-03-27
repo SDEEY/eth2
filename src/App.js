4d07ae15a5459bea39c7ab2ba8cfda39cb598884
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-27 21:02:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = 0
-const image = 'https://pbs.twimg.com/profile_images/1634398226416644103/8jZC3vXu_400x400.jpg'
-const Title = 'YIN YAN REBELLION'
-const supply = 9696
+const ethAmount = 0.7
+const image = 'https://pbs.twimg.com/profile_images/1521924894954598400/aYU4LmuX_400x400.jpg'
+const Title = 'NFZ Zoo Memberships'
+const supply = 400
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
