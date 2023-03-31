82d4eb2d5aa750afae02b2e03d8014a84a3f6ada
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-31 17:42:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0 / 0.008 / 0.022'
-const image = 'https://pbs.twimg.com/profile_images/1617801305015353345/ngsCgHv5_400x400.jpg'
-const Title = 'Cobra Effect'
-const supply = 181
+const ethAmount = '35'
+const image = 'https://pbs.twimg.com/profile_images/1633742588845989889/acs-j8-V_400x400.jpg'
+const Title = 'Watda F*sh'
+const supply = 202
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
