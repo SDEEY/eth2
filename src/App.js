625a0e320643a688b0239916223dd20e3ec52e5d
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-04-07 03:02:22 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.049'
-const image = 'https://pbs.twimg.com/profile_images/1616311280817827841/hXuTmlku_400x400.jpg'
-const Title = 'POG ®, THE GAME YOU COLLECT'
-const supply = 1110
+const ethAmount = '0.1 / 0.12'
+const image = 'https://pbs.twimg.com/profile_images/1635884145254735872/U9OBjHpW_400x400.png'
+const Title = 'Delabs Games'
+const supply = 290
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
