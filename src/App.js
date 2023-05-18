3d7d1c5d6c8618e56940d4a21de74ead7586ee1f
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-05-18 20:32:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.014'
-const image = 'https://pbs.twimg.com/profile_images/1620082970915651585/3ywtun-F_400x400.jpg'
-const Title = 'Seal Squad'
-const supply = 3333
+const ethAmount = '0.0035'
+const image = 'https://pbs.twimg.com/profile_images/1621632126390091778/uRT5y9SY_400x400.jpg'
+const Title = 'Hot Angels'
+const supply = 550
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
