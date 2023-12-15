49352f874278dd7887995305c8f579b8cee24d5d
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-12-15 20:50:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = '0.01'
+const ethAmount = ' 0.022'
 const image = 'https://pbs.twimg.com/profile_images/1531217047828451328/777UrsDy_400x400.jpg'
 const Title = 'Planet Crypto'
-const supply = 111
+const supply = 2500
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
