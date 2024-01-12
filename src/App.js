0ac6448c25c81a41d30015e22585533b813726b3
SDEEY <75689630+SDEEY@users.noreply.github.com>
2024-01-12 20:43:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {useEffect, useState} from "react";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const ethAmount = ' 0.033'
-const image = 'https://pbs.twimg.com/profile_images/1697234742628720640/PpipHhwb_400x400.jpg'
-const Title = 'Hide And Seek'
-const supply = 1333
+const ethAmount = ' 0'
+const image = 'https://pbs.twimg.com/profile_images/1732153234129580032/yz67kzHS_400x400.jpg'
+const Title = 'DADAWGS'
+const supply = 45
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
